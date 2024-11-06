f55897d77a3bdfc6d9d002b77d23461b3bfd2a93
Dmytro Afanasiev <dmytro_afanasiev1@epam.com>
2024-11-06 18:47:57 +0200

    Add nvim

---
+++ .config/nvim/after/plugin/telescope.lua
@@ -0,0 +1,12 @@
+local builtin = require('telescope.builtin')
+-- require('telescope').load_extension('fzf')
+
+
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {}) -- pf: project files
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ps', function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ")})
+end) -- ps: project search
+vim.keymap.set('n', '<leader>pb', builtin.buffers, {}) -- pb: project buffers
+vim.keymap.set('n', '<leader>pg', builtin.live_grep, {}) -- pg: project (live) grep
+
