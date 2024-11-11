02b8faec35588bf715967cdfefa569374d6cda75
Dmytro Afanasiev <dmytro_afanasiev1@epam.com>
2024-11-11 16:11:52 +0200

    Add handlers to pyright

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -50,6 +50,7 @@ require("mason-lspconfig").setup({
                         disableOrganizeImports = true,
                     },
                 },
+                handlers = handlers
             })
         end,
     },
