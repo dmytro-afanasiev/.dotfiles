b813bcfe8e27f0ac783c121a6f1d9c1b9edb001a
Dmytro Afanasiev <dmytro_afanasiev1@epam.com>
2024-12-03 11:18:09 +0200

    Add function for coloring

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,5 +1,8 @@
-vim.cmd.colorscheme('rose-pine')
-vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+function CS(name)
+    vim.cmd.colorscheme(name or 'rose-pine')
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
 
+CS()
 require('nvim-web-devicons').setup()
