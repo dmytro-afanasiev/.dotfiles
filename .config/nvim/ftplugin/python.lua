3e1b4fa21b0c51db9ca10f3c89f147089ffa8561
Dmytro Afanasiev <duma12let@gmail.com>
2026-02-18 13:12:47 +0200

    Add python rt command

--- .config/nvim/ftplugin/python.lua
+++ .config/nvim/ftplugin/python.lua
@@ -2,6 +2,9 @@ vim.treesitter.start()
 vim.keymap.set("n", "<leader>rp", function()
     vim.cmd("split | terminal python " .. vim.fn.expand("%"))
 end, { desc = "Run python in terminal" })
+vim.keymap.set("n", "<leader>rt", function()
+    vim.cmd("split | terminal pytest " .. vim.fn.expand("%"))
+end, {desc = "Run pytest for the current file"})
 
 vim.keymap.set("n", "<leader>t", function()
     local venv = vim.fn.getcwd() .. '/.venv/bin/activate'
